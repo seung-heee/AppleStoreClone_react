4154a245a007e6f76fe284884332e3add0e9df32
seung-heee <tmdgmldi2595@naver.com>
2023-03-02 16:46:52 +0900

    register 구현

--- src/index.js
+++ src/index.js
@@ -3,11 +3,27 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import clayful from 'clayful/client-js';
+
+import axios from 'axios';
+import './css/auth.css';
+
+clayful.config({
+  client: 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6ImU5YjhmZWYxODg4ZWY0NjliZWFhY2FmZDBkNDY1MTMxOGYwMTI0YTk2ODAwMzI4ZWQzNjk5MTQ4Mjg3OGViZWMiLCJyb2xlIjoiY2xpZW50IiwiaWF0IjoxNjc3NTcyMTc5LCJzdG9yZSI6IkhWNzJEQU5SWjJIWS5NSlZVQ0JaVUNSOFciLCJzdWIiOiJRNkwzNUtUNUtSQ1gifQ.hs-WRUhA-1yBxoV-n7vqjqtnPPzjDf6ZvptiyZ-gm_4'
+});
+
+clayful.install('request', require('clayful/plugins/request-axios')(axios));
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
